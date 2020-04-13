3d9a1a11b47e3e1b86bcfc15b8a0d8cd046011ce
Lindsay Mecher <lindsaymecher@gmail.com>
2020-04-13 17:45:55 -0500

    started Bootstrap restyling components

---
+++ src/components/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Jumbotron as Jumbo, Container, Card } from 'react-bootstrap';
+import styled from 'styled-components';
+import music_notes from '../music_notes.png';
+
+const Styles = styled.div`
+    .jumbo {
+        background: url(${music_notes}) no-repeat fixed bottom;
+        background-size: cover;
+        height: 50px;
+        position: relative;
+        z-index: -2;
+
+    }
+    .overlay {
+        background-color: black;
+        opacity: 0.8;
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        right: 0;
+        z-index: -1;
+    }
+`;
+
+const Footer = () => (
+    <Styles>
+        <Jumbo fluid={true} className="jumbo">
+            <div className="overlay">
+            
+            </div>
+            <Container>
+                <img src="" alt="" />    
+            </Container>
+        </Jumbo>
+    </Styles>
+);
+
+export default Footer;
