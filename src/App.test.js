551a29968230718da4d0cd090916ae687da39dbd
Lindsay Mecher <lindsaymecher@gmail.com>
2025-10-18 12:01:40 -0500

    (MS2) Configure Prettier. Update the test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { render } from "@testing-library/react";
-import App from "./App";
+import Home from "./components/Home";
 
 test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const { getByText } = render(<Home />);
+  const linkElement = getByText(/Classique/i);
   expect(linkElement).toBeInTheDocument();
 });
