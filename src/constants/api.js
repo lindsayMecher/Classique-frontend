cf6a734d9fddd15d6f2ea48193abcbe6e43571b2
Lindsay Mecher <lindsaymecher@gmail.com>
2025-10-18 10:44:55 -0500

    (MS2) Configure linter. Start resolving errors

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,9 +1,9 @@
 export const LOCALHOST_API = "http://localhost:3000";
 
 export const ENDPOINTS = {
-    USERS: "/users",
-    POSTS: "/posts",
-    FAVORITES: "/favorites",
-    CURRENT_USER: "/current_user",
-    AUTH: "/auth"
+  USERS: "/users",
+  POSTS: "/posts",
+  FAVORITES: "/favorites",
+  CURRENT_USER: "/current_user",
+  AUTH: "/auth"
 }
