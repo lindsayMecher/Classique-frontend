0b212e0504e4e7ee8425f3ae215841386a9a35ac
Lindsay Mecher <lindsaymecher@gmail.com>
2025-11-02 23:38:55 -0600

    (MS6) Fix New Post UI minus radio

--- src/components/New.js
+++ src/components/New.js
@@ -12,10 +12,20 @@ const Styles = styled.div`
 
   .fave-btn {
     background-color: #612da1;
+    margin: 3rem;
     &:hover {
       background-color: black;
       color: white;
     }
+  }
+
+  .main-header {
+    padding: 5rem;
+  }
+
+  .form-label {
+    text-align: left !important;
+    display: block;
   }
 `;
 
@@ -65,13 +75,8 @@ function New({ updateUser, handleNewPost }) {
 
   return (
     <Styles>
-      <div className="container">
-        <br />
-        <br />
-        <h1 className="headers">New Post</h1>
-        <br />
-        <br />
-        <br />
+      <div className="headers">
+        <h1 className="main-header">New Post</h1>
         <Form
           onSubmit={(event) =>
             handleNewPost(event, {
@@ -91,13 +96,14 @@ function New({ updateUser, handleNewPost }) {
             })
           }
         >
-          <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="opportunityDropdown">
-                <Form.Label>Type of Opportunity</Form.Label>
+          <Row className="mb-3">
+            <Col xs={12} md={6}>
+              <Form.Group controlId="opportunityDropdown">
+                <Form.Label className="mb-1">Type of Opportunity</Form.Label>
                 <Form.Select
                   onChange={(e) => setPerformanceType(e.target.value)}
                   name="performance_type"
+                  className="w-100 form-control"
                 >
                   <option value="Concert">Concert</option>
                   <option value="Rehearsal">Rehearsal</option>
@@ -111,12 +117,13 @@ function New({ updateUser, handleNewPost }) {
                 </Form.Select>
               </Form.Group>
             </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="voiceTypeDropdown">
-                <Form.Label>Voice Type</Form.Label>
+            <Col xs={12} md={6}>
+              <Form.Group controlId="voiceTypeDropdown">
+                <Form.Label className="mb-1">Voice Type</Form.Label>
                 <Form.Select
                   onChange={(e) => setVoiceType(e.target.value)}
                   name="voice_type"
+                  className="w-100 form-control"
                 >
                   <option value="Soprano">Soprano</option>
                   <option value="Mezzo-Soprano">Mezzo-Soprano</option>
@@ -131,169 +138,175 @@ function New({ updateUser, handleNewPost }) {
               </Form.Group>
             </Col>
           </Row>
-          <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formGridDate">
-                <Form.Label>Date</Form.Label>
+          <Row className="mb-3">
+            <Col xs={12} md={3}>
+              <Form.Group controlId="formGridDate">
+                <Form.Label className="mb-1">Date</Form.Label>
                 <Form.Control
                   onChange={(e) => setDate(e.target.value)}
                   type="date"
                   name="date"
                   value={date}
+                  className="w-100"
                 />
               </Form.Group>
             </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="formGridTime">
-                <Form.Label>Time</Form.Label>
+            <Col xs={12} md={3}>
+              <Form.Group controlId="formGridTime">
+                <Form.Label className="mb-1">Time</Form.Label>
                 <Form.Control
                   onChange={(e) => setTime(e.target.value)}
                   type="time"
                   name="time"
                   value={time}
+                  className="w-100"
                 />
               </Form.Group>
             </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formGridVenueName">
-                <Form.Label>Venue Name</Form.Label>
+            <Col xs={12} md={6}>
+              <Form.Group controlId="formGridVenueName">
+                <Form.Label className="mb-1">Venue Name</Form.Label>
                 <Form.Control
                   onChange={(e) => setVenueName(e.target.value)}
                   type="text"
                   name="venue_name"
                   value={venueName}
                   placeholder="Enter venue name..."
+                  className="w-100"
                 />
               </Form.Group>
             </Col>
           </Row>
-          <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formGridAddress1">
-                <Form.Label>Address Line One</Form.Label>
+          <Row className="mb-3">
+            <Col xs={12} md={5}>
+              <Form.Group controlId="formGridAddress1">
+                <Form.Label className="mb-1">Address Line One</Form.Label>
                 <Form.Control
                   onChange={(e) => setStreetAddress(e.target.value)}
                   type="text"
                   name="street_address"
                   value={streetAddress}
                   placeholder="Enter street address..."
+                  className="w-100"
                 />
               </Form.Group>
             </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="formGridAddress2">
-                <Form.Label>Address Line Two</Form.Label>
+            <Col xs={12} md={5}>
+              <Form.Group controlId="formGridAddress2">
+                <Form.Label className="mb-1">Address Line Two</Form.Label>
                 <Form.Control
                   onChange={(e) => setStreetAddress2(e.target.value)}
                   type="text"
                   name="address_line_two"
                   value={streetAddress2}
                   placeholder="Enter apartment number...(optional)"
+                  className="w-100"
                 />
               </Form.Group>
             </Col>
-          </Row>
-          <Row>
-            <Col>
-              <Form.Group className="mb-3" controlId="formGridCity">
-                <Form.Label>City</Form.Label>
-                <Form.Control
-                  onChange={(e) => setCity(e.target.value)}
-                  type="text"
-                  name="city"
-                  value={city}
-                  placeholder="Enter city..."
-                />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="formGridState">
-                <Form.Label>State</Form.Label>
-                <Form.Control
-                  onChange={(e) => setState(e.target.value)}
-                  type="text"
-                  name="state"
-                  value={state}
-                  placeholder="Enter state..."
-                />
-              </Form.Group>
-            </Col>
-            <Col>
-              <Form.Group className="mb-3" controlId="formGridZip">
-                <Form.Label>Zip Code</Form.Label>
+            <Col xs={12} md={2}>
+              <Form.Group controlId="formGridZip">
+                <Form.Label className="mb-1">Zip Code</Form.Label>
                 <Form.Control
                   onChange={(e) => setZip(e.target.value)}
                   type="text"
                   name="zip"
                   value={zip}
                   placeholder="Enter zip code..."
+                  className="w-100"
                 />
               </Form.Group>
             </Col>
           </Row>
-          <Col>
-            <Form.Group controlId="formGridRepertoire">
-              <Form.Label>Repertoire</Form.Label>
-              <Form.Control
-                as="textarea"
-                onChange={(e) => setRepertoire(e.target.value)}
-                name="repertoire"
-                rows="8"
-                value={repertoire}
-                placeholder="Enter repertoire..."
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="formGridNotes">
-              <Form.Label>Additional Notes</Form.Label>
-              <Form.Control
-                as="textarea"
-                onChange={(e) => setNotes(e.target.value)}
-                name="notes"
-                rows="8"
-                value={notes}
-                placeholder="Enter notes like: 'Dress code is concert wear, knee-length dress, please arrive 15 minutes early, must have experience cantoring a Catholic mass, etc.' "
-              />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group>
-              <Form.Label>Compensation</Form.Label>
-              <div className="mb-3">
-                <Form.Check
-                  type="radio"
-                  id={"paid"}
-                  label={"Paid"}
-                  checked={paid === true}
-                  onChange={togglePaid}
-                  name="paid"
-                  value="true"
+          <Row className="mb-3">
+            <Col xs={12} md={5}>
+              <Form.Group controlId="formGridCity">
+                <Form.Label className="mb-1">City</Form.Label>
+                <Form.Control
+                  onChange={(e) => setCity(e.target.value)}
+                  type="text"
+                  name="city"
+                  value={city}
+                  placeholder="Enter city..."
+                  className="w-100"
                 />
-                <Form.Check
-                  type="radio"
-                  id={"unpaid"}
-                  label={"Unpaid"}
-                  checked={paid === false}
-                  onChange={togglePaid}
-                  name="paid"
-                  value="false"
+              </Form.Group>
+            </Col>
+            <Col xs={12} md={5}>
+              <Form.Group controlId="formGridState">
+                <Form.Label className="mb-1">State</Form.Label>
+                <Form.Control
+                  onChange={(e) => setState(e.target.value)}
+                  type="text"
+                  name="state"
+                  value={state}
+                  placeholder="Enter state..."
+                  className="w-100"
                 />
-              </div>
-            </Form.Group>
-          </Col>
-          <br />
-          <br />
+              </Form.Group>
+            </Col>
+            <Col xs={12} md={2}>
+              <Form.Group>
+                <Form.Label className="mb-1">Compensation</Form.Label>
+                <div className="mb-3">
+                  <Form.Check
+                    type="radio"
+                    id={"paid"}
+                    label={"Paid"}
+                    checked={paid === true}
+                    onChange={togglePaid}
+                    name="paid"
+                    value="true"
+                  />
+                  <Form.Check
+                    type="radio"
+                    id={"unpaid"}
+                    label={"Unpaid"}
+                    checked={paid === false}
+                    onChange={togglePaid}
+                    name="paid"
+                    value="false"
+                  />
+                </div>
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col xs={12} md={12}>
+              <Form.Group controlId="formGridRepertoire">
+                <Form.Label className="mb-1">Repertoire</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  onChange={(e) => setRepertoire(e.target.value)}
+                  name="repertoire"
+                  rows="8"
+                  value={repertoire}
+                  placeholder="Enter repertoire..."
+                  className="w-100"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="mb-3">
+            <Col xs={12} md={12}>
+              <Form.Group controlId="formGridNotes">
+                <Form.Label className="mb-1">Additional Notes</Form.Label>
+                <Form.Control
+                  as="textarea"
+                  onChange={(e) => setNotes(e.target.value)}
+                  name="notes"
+                  rows="8"
+                  value={notes}
+                  className="w-100"
+                  placeholder="Enter notes like: 'Dress code is concert wear, knee-length dress, please arrive 15 minutes early, must have experience cantoring a Catholic mass, etc.' "
+                />
+              </Form.Group>
+            </Col>
+          </Row>
           <Button className="btn btn-secondary btn-lg fave-btn" type="submit">
             Post
           </Button>
         </Form>
-        <br />
-        <br />
-        <br />
-        <br />
       </div>
     </Styles>
   );
